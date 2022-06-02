7c3f6f3131d85179a646827ef5f09049dcf6a968
mmccarthy64 <mmccarthy64@hotmail.com>
2022-06-02 10:52:35 -0500

    display data to table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,28 +1,29 @@
 import React from 'react'
 
-export default function Table(props) {
+const Table = (props) => {
     const returns = props.returns
-  return (
-    <div className='table'>
-        <table>
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Return</th>
-            <th>Cumulative Returns</th>
-          </tr>
-        </thead>
-        <tbody>
-            {returns.map(data => {
-                // console.log(year)
-                <tr>
-                    <td>{data.year}</td>
-                    <td>{data.totalReturn}</td>
-                </tr>
-                // {console.log(data.totalReturn)}
-            })}
-        </tbody>
-      </table>
-    </div>
-  )
+
+    return (
+        <div className='table'>
+            <table>
+            <thead>
+            <tr>
+                <th>Year</th>
+                <th>Total Return</th>
+                <th>Cumulative Returns</th>
+            </tr>
+            </thead>
+            <tbody>
+                {returns.map((data) => (
+                    <tr>
+                        <td>{data.year}</td>
+                        <td>{data.totalReturn}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        </div>
+    )
 }
+
+export default Table;

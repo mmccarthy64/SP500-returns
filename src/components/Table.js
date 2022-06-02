1865a411c50d27a73b003d2256f701bf92d9e5bd
mmccarthy64 <mmccarthy64@hotmail.com>
2022-06-02 09:59:31 -0500

    add table component

---
+++ src/components/Table.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Table() {
+  return (
+    <div className='table'>
+        <table>
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Total Return</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>2020</th>
+            <th>18.40</th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
